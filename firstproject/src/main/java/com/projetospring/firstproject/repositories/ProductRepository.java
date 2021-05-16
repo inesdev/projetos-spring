2ccb67d7954430bc3ff7b106ab8c0f6526f33741
inesdev <inessouzasp@gmail.com>
2021-05-16 20:49:47 -0300

    Project Spring

---
+++ firstproject/src/main/java/com/projetospring/firstproject/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.projetospring.firstproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projetospring.firstproject.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {	
+	
+}
